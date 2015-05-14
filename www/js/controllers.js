e8b1b8e0c5b679e6f920d6633714f7ca8cbbf9b4
Denni Adam <dennila2@ya.ru>
2015-05-14 22:22:58 +0300

    work

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,37 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function ($scope, $timeout) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+    $scope.al = {
+      title: 'I am a title',
+      message: 'I am great message',
+      button: 'My name is Button'
+      };
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    $scope.succ = function () {
+      console.log('good job');
+      $scope.success = 'good job!';
+    };
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+        $scope.alert = function () {
+      console.log($scope.al.title, $scope.al.message, $scope.al.button);
+      window.Alert.alert($scope.al.title, $scope.al.message, $scope.al.button, function(){ $timeout($scope.succ)});
+    }
+  })
+
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    }
+  })
+
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
